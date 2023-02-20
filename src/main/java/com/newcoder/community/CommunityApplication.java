2c70fbe14a56cbf29efa855a92884ec9542e00b7
huangtao <738659456@qq.com>
2023-02-21 01:52:48 +0800

    "update"

--- src/main/java/com/newcoder/community/CommunityApplication.java
+++ src/main/java/com/newcoder/community/CommunityApplication.java
@@ -6,7 +6,7 @@ import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 public class CommunityApplication {
-
+	//主启动类
 	public static void main(String[] args) {
 		SpringApplication.run(CommunityApplication.class, args);
 	}
